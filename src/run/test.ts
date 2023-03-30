3e3068286c480b1e9ea35727f54d96201ce0ba3d
Vitaliy Potapov <noginsk@rambler.ru>
2023-03-30 17:56:02 +0400

    world: pass options to constructor

--- src/run/test.ts
+++ src/run/test.ts
@@ -2,20 +2,11 @@ import { test as base } from '@playwright/test';
 import { getSupportCodeLibrary, invokeStep } from './support';
 
 export const test = base.extend({
-  world: async ({ page, context, browser, browserName, request }, use) => {
+  world: async (fixtures, use) => {
     const support = await getSupportCodeLibrary();
-    const world = new support.World();
+    const world = new support.World(fixtures);
     // Todo: support world parameters
     // See: https://github.com/cucumber/cucumber-js/blob/764b7b6be4ccaa235954acb6424fb9725df6a643/src/runtime/test_case_runner.ts#L96
-
-    // Set props directly to keep constructor signature for user
-    // See: https://playwright.dev/docs/test-fixtures#built-in-fixtures
-    world.page = page;
-    world.context = context;
-    world.browser = browser;
-    world.browserName = browserName;
-    world.request = request;
-
     await use(world);
   },
 
--- src/run/world.ts
+++ src/run/world.ts
@@ -1,6 +1,40 @@
-import { Page } from '@playwright/test';
+import {
+  APIRequestContext,
+  Browser,
+  BrowserContext,
+  Page,
+} from '@playwright/test';
 
 // See: https://playwright.dev/docs/test-fixtures#built-in-fixtures
+export type WorldOptions = {
+  page: Page;
+  context: BrowserContext;
+  browser: Browser;
+  browserName: string;
+  request: APIRequestContext;
+};
+
+// todo: inherit from cucumber's World?
 export class World {
-  page!: Page;
+  constructor(private options: WorldOptions) {}
+
+  get page() {
+    return this.options.page;
+  }
+
+  get context() {
+    return this.options.context;
+  }
+
+  get browser() {
+    return this.options.browser;
+  }
+
+  get browserName() {
+    return this.options.browserName;
+  }
+
+  get request() {
+    return this.options.request;
+  }
 }
