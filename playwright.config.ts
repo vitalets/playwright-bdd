0e0012af385fd83e3ba89f5831a83399da80a69e
Vitaliy Potapov <noginsk@rambler.ru>
2023-05-20 14:24:02 +0400

    forbid only for several test runs

--- playwright.config.ts
+++ playwright.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
+  forbidOnly: Boolean(process.env.FORBID_ONLY),
   projects: [
     { name: 'generated-tests', testDir: '.features-gen', testIgnore: 'only-skip-fixme.feature.spec.js' },
     { name: 'parse-error', testDir: 'test/parse-error' },
