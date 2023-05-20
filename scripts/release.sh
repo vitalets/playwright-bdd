a7ef46525d10598d35bd05f24e5a32207a2da59c
Vitaliy Potapov <noginsk@rambler.ru>
2023-05-20 14:24:02 +0400

    forbid only for several test runs

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 npm run lint
 npm run prettier
-npm test
+FORBID_ONLY=1 npm test
 npm run examples:b # 'npm run build' is called inside examples:b
 npm run examples
 npx np --yolo --no-release-draft
--- test/pwstyle/playwright.config.ts
+++ test/pwstyle/playwright.config.ts
@@ -2,4 +2,5 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   testDir: '.features-gen',
+  forbidOnly: Boolean(process.env.FORBID_ONLY),
 });
