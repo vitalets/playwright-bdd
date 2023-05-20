88c4deb05c29c58d83e3eeb4a2f4a992d6dbf9da
Vitaliy Potapov <noginsk@rambler.ru>
2023-05-20 15:14:23 +0400

    allow release from any branch

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,4 +8,4 @@ npm run prettier
 FORBID_ONLY=1 npm test
 npm run examples:b # 'npm run build' is called inside examples:b
 npm run examples
-npx np --yolo --no-release-draft
+npx np --yolo --no-release-draft --any-branch
