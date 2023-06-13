f46cfbe1420069a074297e90b8efe27a6fa5a8b3
Vitaliy Potapov <noginsk@rambler.ru>
2023-06-13 12:54:01 +0400

    cli: support --verbose

--- src/gen/cli.ts
+++ src/gen/cli.ts
@@ -5,6 +5,7 @@ import { generateTestFiles } from '.';
 import { exitWithMessage } from '../utils';
 import { loadConfig as loadPlaywrightConfig } from '../playwright/loadConfig';
 import { getEnvConfigs } from '../config/env';
+import { BDDConfig } from '../config';
 
 const program = new Command();
 
@@ -13,18 +14,26 @@ program
   .description('Generate Playwright tests from Gherkin documents')
   .option(
     '-c, --config <file>',
-    `Path to Playwright configuration file. Default: playwright.config.(js|ts)`,
+    `Path to Playwright configuration file (default: playwright.config.(js|ts))`,
   )
+  .option('--verbose', `Verbose mode (default: false)`)
   .action(async (opts) => {
     await loadPlaywrightConfig(opts.config);
     const configs = Object.values(getEnvConfigs());
     assertConfigsExist(configs);
+    const cliConfig = buildCliConfig(opts);
     for (const config of configs) {
-      await generateTestFiles(config);
+      await generateTestFiles({ ...config, ...cliConfig });
     }
   });
 
 program.parse();
+
+function buildCliConfig(opts: { verbose?: boolean }) {
+  const config: Partial<BDDConfig> = {};
+  if ('verbose' in opts) config.verbose = opts.verbose;
+  return config;
+}
 
 function assertConfigsExist(configs: unknown[]) {
   if (configs.length === 0) {
