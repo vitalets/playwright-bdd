1ecee6987975c9652c65b03bd91b140ec8f52358
Vitaliy Potapov <noginsk@rambler.ru>
2023-06-14 16:43:25 +0400

    use cross env for envs

--- scripts/release.sh
+++ scripts/release.sh
@@ -5,7 +5,7 @@ set -euo pipefail
 
 npm run lint
 npm run prettier
-FORBID_ONLY=1 npm test
+npx cross-env FORBID_ONLY=1 npm test
 npm run examples:b # 'npm run build' is called inside examples:b
 npm run examples
 npx np --yolo --no-release-draft --no-tests --any-branch
