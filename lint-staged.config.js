761a864ac88048db57c19450433f45eb17ff9070
Vitaliy Potapov <noginsk@rambler.ru>
2023-06-14 13:59:07 +0400

    lint-staged fix

--- lint-staged.config.js
+++ lint-staged.config.js
@@ -2,7 +2,7 @@ module.exports = {
   '**/*.{js,ts}': [
     'eslint --fix',
     'prettier --write --ignore-unknown',
-    (files) => (files.length ? 'cross-env FORBID_ONLY=1 npm test' : undefined),
+    (files) => (files.length ? 'cross-env FORBID_ONLY=1 npm test' : ''),
   ],
   '!(**/*.{js,ts})': 'prettier --write --ignore-unknown',
 };
