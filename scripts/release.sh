979015f78881cb6d59f5a5eab36aac6b30a5d40b
Vitaliy Potapov <noginsk@rambler.ru>
2023-06-28 11:16:06 +0400

    examples: run from node instead of shell

--- scripts/release.sh
+++ scripts/release.sh
@@ -8,7 +8,6 @@ npm ci
 npm run lint
 npm run prettier
 npm run knip
-npx cross-env FORBID_ONLY=1 npm test
-npm run examples:b # 'npm run build' is called inside examples:b
+npx cross-env FORBID_ONLY=1 npm test # 'npm run build' is called inside
 npm run examples
 npx np --yolo --no-release-draft --no-tests --any-branch
