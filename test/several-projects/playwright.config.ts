fabbf8f3bcb22818898c87c5caec577e24477461
Vitaliy Potapov <noginsk@rambler.ru>
2023-07-26 18:52:54 +0400

    test: improve several-projects test

--- test/several-projects/playwright.config.ts
+++ test/several-projects/playwright.config.ts
@@ -4,7 +4,7 @@ import { defineBddConfig } from '../../dist';
 export default defineConfig({
   projects: [
     {
-      name: 'project one',
+      name: 'project-one',
       testDir: defineBddConfig({
         outputDir: '.features-gen/one',
         importTestFrom: 'one/steps/fixtures.ts',
@@ -13,13 +13,24 @@ export default defineConfig({
       }),
     },
     {
-      name: 'project two',
+      name: 'project-two',
       testDir: defineBddConfig({
         outputDir: '.features-gen/two',
         importTestFrom: 'two/steps/fixtures.ts',
         paths: ['two/*.feature'],
         require: ['one/steps/*.ts', 'two/steps/*.ts'],
       }),
+      dependencies: ['project-one'],
+    },
+    {
+      name: 'project-three',
+      testDir: defineBddConfig({
+        outputDir: '.features-gen/three',
+        importTestFrom: 'two/steps/fixtures.ts',
+        paths: ['two/*.feature'],
+        require: ['one/steps/*.ts', 'two/steps/*.ts'],
+      }),
+      dependencies: ['project-one'],
     },
   ],
   forbidOnly: Boolean(process.env.FORBID_ONLY),
