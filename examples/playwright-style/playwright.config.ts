23af86418fa2fafd1c8e7270046c079fe42982ce
Vitaliy Potapov <noginsk@rambler.ru>
2023-07-31 10:46:22 +0400

    examples: use backtick

--- examples/playwright-style/playwright.config.ts
+++ examples/playwright-style/playwright.config.ts
@@ -5,6 +5,7 @@ const testDir = defineBddConfig({
   importTestFrom: 'steps/fixtures.ts',
   paths: ['features'],
   require: ['steps/*.ts'],
+  quotes: 'backtick',
 });
 
 export default defineConfig({
