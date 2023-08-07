e41ac7b82ce04a364564a0bf6c28090f3f9f0a9f
Vitaliy Potapov <noginsk@rambler.ru>
2023-08-07 12:40:54 +0400

    knip

--- knip.config.ts
+++ knip.config.ts
@@ -1,13 +1,14 @@
 import type { KnipConfig } from 'knip';
 
 const config: KnipConfig = {
-  entry: ['src/cli/index.ts', 'src/gen/worker.ts', 'src/index.ts', 'src/decorators.ts'],
+  entry: ['src/cli/index.ts', 'src/cli/worker.ts', 'src/index.ts', 'src/decorators.ts'],
   project: ['src/**/*.ts'],
   ignore: ['**/*.d.ts'],
   ignoreDependencies: [
     '@cucumber/messages',
     '@cucumber/cucumber-expressions',
     '@cucumber/gherkin',
+    '@cucumber/tag-expressions',
     'lint-staged',
     'np',
     'npm-run-all',
--- scripts/release.sh
+++ scripts/release.sh
@@ -4,10 +4,10 @@
 set -euo pipefail
 
 npx publint
+npm run knip
 npm ci
 npm run lint
 npm run prettier
-npm run knip
 npx cross-env FORBID_ONLY=1 npm test # 'npm run build' is called inside
 npm run examples
 SKIP_PRE_PUSH=1 npx np --yolo --no-release-draft --no-tests --any-branch
