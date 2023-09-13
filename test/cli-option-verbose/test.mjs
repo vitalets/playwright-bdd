25ae3e52e78d2b4237ff7df95a856e63b006e085
Vitaliy Potapov <noginsk@rambler.ru>
2023-09-13 18:17:00 +0400

    fix win warnings

--- test/cli-option-verbose/test.mjs
+++ test/cli-option-verbose/test.mjs
@@ -1,3 +1,4 @@
+import path from 'node:path';
 import { expect } from '@playwright/test';
 import { test, execPlaywrightTest, TestDir } from '../helpers.mjs';
 
@@ -7,6 +8,8 @@ test(testDir.name, () => {
   const stdout = execPlaywrightTest(testDir.name, 'node ../../dist/cli --verbose');
   expect(stdout).toContain('Loading features from: features/*.feature');
   expect(stdout).toContain('Loading steps from: steps.ts');
-  expect(stdout).toContain('Generated: .features-gen/features/sample.feature.spec.js');
+  expect(stdout).toContain(
+    `Generated: ${path.normalize('.features-gen/features/sample.feature.spec.js')}`,
+  );
   expect(stdout).toContain('Generated files: 1');
 });
--- test/decorators-unsure-fixtures/test.mjs
+++ test/decorators-unsure-fixtures/test.mjs
@@ -9,7 +9,7 @@ test(testDir.name + ' (guess)', () =>
       `Can't guess fixture for decorator step "BasePage: step" in file: features/guess.feature`,
       `Please refactor your Page Object classes or set one of the following tags: @fixture:todoPage, @fixture:todoPage2`,
     ],
-    `FEATURE=guess.feature ${DEFAULT_CMD}`,
+    `npx cross-env FEATURE=guess.feature ${DEFAULT_CMD}`,
   ),
 );
 
@@ -17,7 +17,7 @@ test(testDir.name + ' (scenario-tag)', () =>
   execPlaywrightTestWithError(
     testDir.name,
     [`contains 1 step(s) not compatible with required fixture "todoPage"`],
-    `FEATURE=scenario-tag.feature ${DEFAULT_CMD}`,
+    `npx cross-env FEATURE=scenario-tag.feature ${DEFAULT_CMD}`,
   ),
 );
 
@@ -25,7 +25,7 @@ test(testDir.name + ' (feature-tag)', () =>
   execPlaywrightTestWithError(
     testDir.name,
     [`contains 1 step(s) not compatible with required fixture "todoPage"`],
-    `FEATURE=feature-tag.feature ${DEFAULT_CMD}`,
+    `npx cross-env FEATURE=feature-tag.feature ${DEFAULT_CMD}`,
   ),
 );
 
@@ -33,6 +33,6 @@ test(testDir.name + ' (bg)', () =>
   execPlaywrightTestWithError(
     testDir.name,
     [`Scenario "Background" contains 1 step(s) not compatible with required fixture "todoPage"`],
-    `FEATURE=bg.feature ${DEFAULT_CMD}`,
+    `npx cross-env FEATURE=bg.feature ${DEFAULT_CMD}`,
   ),
 );
