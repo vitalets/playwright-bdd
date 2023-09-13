1ef2f6d448c05cb56311294c2bdc28687db010c1
Vitaliy Potapov <noginsk@rambler.ru>
2023-09-13 19:19:03 +0400

    only

--- test/esm/test.mjs
+++ test/esm/test.mjs
@@ -1,3 +1,3 @@
 import { test, getTestName, execPlaywrightTest } from '../helpers.mjs';
 
-test.skip(getTestName(import.meta), (t) => execPlaywrightTest(t.name));
+test.only(getTestName(import.meta), (t) => execPlaywrightTest(t.name));
