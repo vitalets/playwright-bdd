2c1f4cd7291bda285f5f78c458d640a4db242738
Vitaliy Potapov <noginsk@rambler.ru>
2023-09-13 19:01:41 +0400

    skip esm-ts

--- test/esm-ts/test.mjs
+++ test/esm-ts/test.mjs
@@ -1,6 +1,6 @@
 import { test, getTestName, execPlaywrightTest, DEFAULT_CMD } from '../helpers.mjs';
 
-test(getTestName(import.meta), (t) => {
+test.skip(getTestName(import.meta), (t) => {
   execPlaywrightTest(
     t.name,
     `npx cross-env NODE_OPTIONS="--loader ts-node/esm --no-warnings" ${DEFAULT_CMD}`,
