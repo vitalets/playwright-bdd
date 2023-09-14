3e9ea90e6dc608b2be6157e925e4e31213c0ca8e
Vitaliy Potapov <noginsk@rambler.ru>
2023-09-14 21:30:54 +0400

    test: simplify quotes test

--- test/quotes/playwright.config.ts
+++ test/quotes/playwright.config.ts
@@ -1,14 +1,35 @@
 import { defineConfig } from '@playwright/test';
-import { defineBddConfig, BDDInputConfig } from '../../dist';
-
-const testDir = defineBddConfig({
-  quotes: process.env.QUOTES as BDDInputConfig['quotes'],
-  outputDir: `.features-gen/${process.env.QUOTES}`,
-  paths: ['*.feature'],
-  require: ['steps.ts'],
-});
+import { defineBddConfig } from '../../dist';
 
 export default defineConfig({
-  testDir,
+  projects: [
+    {
+      name: 'single-qoutes',
+      testDir: defineBddConfig({
+        quotes: 'single',
+        outputDir: `.features-gen/single`,
+        paths: ['*.feature'],
+        require: ['steps.ts'],
+      }),
+    },
+    {
+      name: 'double-qoutes',
+      testDir: defineBddConfig({
+        quotes: 'double',
+        outputDir: `.features-gen/double`,
+        paths: ['*.feature'],
+        require: ['steps.ts'],
+      }),
+    },
+    {
+      name: 'backtick-quotes',
+      testDir: defineBddConfig({
+        quotes: 'backtick',
+        outputDir: `.features-gen/backtick`,
+        paths: ['*.feature'],
+        require: ['steps.ts'],
+      }),
+    },
+  ],
   forbidOnly: Boolean(process.env.FORBID_ONLY),
 });
