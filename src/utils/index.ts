3880411b1a4ca795b79a7e845940447cb829900b
Vitaliy Potapov <noginsk@rambler.ru>
2023-11-23 10:03:17 +0400

    comment

--- src/utils/index.ts
+++ src/utils/index.ts
@@ -2,7 +2,10 @@ import fs from 'node:fs';
 import path from 'node:path';
 import timers from 'node:timers/promises';
 
-// See: https://stackoverflow.com/questions/50453640/how-can-i-get-the-value-of-a-symbol-property
+/**
+ * Returns Symbol by name.
+ * See: https://stackoverflow.com/questions/50453640/how-can-i-get-the-value-of-a-symbol-property
+ */
 export function getSymbolByName<T extends object>(target: T, name?: string) {
   const ownKeys = Reflect.ownKeys(target);
   const symbol = ownKeys.find((key) => key.toString() === `Symbol(${name})`);
