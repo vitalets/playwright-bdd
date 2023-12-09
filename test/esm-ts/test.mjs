2e125afc805bd08cdfacfa1de8d87fda4a6b654e
Vitaliy Potapov <noginsk@rambler.ru>
2023-12-09 15:15:37 +0400

    test: fix esm-ts for pw < 1.35

--- test/esm-ts/test.mjs
+++ test/esm-ts/test.mjs
@@ -14,9 +14,9 @@ test(getTestName(import.meta), (t) => {
     `npx cross-env NODE_OPTIONS="--loader ts-node/esm --no-warnings" ${DEFAULT_CMD}`,
   );
 
-  // on pw <= 1.34 for esm-ts location is incorrect (:9:11 instead of :7:11)
+  // on pw < 1.35 for esm-ts location is incorrect (:9:11 instead of :7:11)
   // needs investigation or eventually update minimal pw version
-  if (getPackageVersion('@playwright/test') <= '1.34') return;
+  if (getPackageVersion('@playwright/test') < '1.35') return;
 
   expect(stdout).toContain(
     `Given State 1 ${normalize('.features-gen/one/sample.feature.spec.js')}:7:11`,
