c4659248a5ee88a32e591ff8030a66d47744005f
Vitaliy Potapov <noginsk@rambler.ru>
2024-01-17 14:58:20 +0400

    finalize html reporter

--- src/reporter/cucumber/htmlReporter.ts
+++ src/reporter/cucumber/htmlReporter.ts
@@ -2,25 +2,41 @@
  * Cucumber html reporter.
  * See: https://github.com/cucumber/cucumber-js/blob/main/src/formatter/html_formatter.ts
  */
-import * as messages from '@cucumber/messages';
+import { finished } from 'node:stream/promises';
+import CucumberHtmlStream from '@cucumber/html-formatter';
 import { BaseReporter, BaseReporterOptions } from './baseReporter';
+import { resolvePackageRoot } from '../../utils';
+import path from 'node:path';
 
 export type HtmlReporterOptions = {
   outputFile?: string;
 };
 
 export class HtmlReporter extends BaseReporter {
+  private htmlStream: CucumberHtmlStream;
+
   constructor(
     baseOptions: BaseReporterOptions,
     protected options: HtmlReporterOptions = {},
   ) {
     super(baseOptions);
-    // this.outputStream = new CucumberHtmlStream(
-    //   resolvePkg('@cucumber/html-formatter', { cwd: __dirname }) + '/dist/main.css',
-    //   resolvePkg('@cucumber/html-formatter', { cwd: __dirname }) + '/dist/main.js',
-    // );
-    baseOptions.eventBroadcaster.on('envelope', (envelope: messages.Envelope) => {
-      this.outputStream.write(envelope);
+    if (this.options.outputFile) {
+      this.setOutputStreamToFile(this.options.outputFile);
+    }
+    const packageRoot = resolvePackageRoot('@cucumber/html-formatter');
+    this.htmlStream = new CucumberHtmlStream(
+      path.join(packageRoot, 'dist/main.css'),
+      path.join(packageRoot, 'dist/main.js'),
+    );
+    this.eventBroadcaster.on('envelope', (envelope) => {
+      this.htmlStream.write(envelope);
     });
+    this.htmlStream.pipe(this.outputStream);
+  }
+
+  async finished() {
+    this.htmlStream.end();
+    await finished(this.htmlStream);
+    await super.finished();
   }
 }
