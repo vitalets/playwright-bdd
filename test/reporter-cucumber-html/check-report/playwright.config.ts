20010a9b32923ae3b50c21fa7a4857921469ff21
Vitaliy Potapov <noginsk@rambler.ru>
2024-01-21 14:38:57 +0400

    test: test screenshot in html reporter

--- test/reporter-cucumber-html/check-report/playwright.config.ts
+++ test/reporter-cucumber-html/check-report/playwright.config.ts
@@ -5,6 +5,8 @@ import { defineConfig } from '@playwright/test';
 
 export default defineConfig({
   expect: { timeout: 500 },
+  outputDir: './test-results',
+  reporter: 'dot',
   use: {
     screenshot: 'only-on-failure',
   },
