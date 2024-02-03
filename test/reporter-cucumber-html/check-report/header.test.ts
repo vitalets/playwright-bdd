ec04a839f76af787a4fc97677078964618aef07c
Vitaliy Potapov <noginsk@rambler.ru>
2024-02-03 19:23:31 +0400

    test(reporter): feature tags

--- test/reporter-cucumber-html/check-report/header.test.ts
+++ test/reporter-cucumber-html/check-report/header.test.ts
@@ -1,17 +1,17 @@
 import { test, expect } from '@playwright/test';
-import { openReport } from './helpers';
+import { getFeature, openReport } from './helpers';
 
 test.beforeEach(async ({ page }) => {
   await openReport(page);
 });
 
-test('passed / failed / executed', async ({ page }) => {
+test('header info', async ({ page }) => {
   await expect(page.getByText('8 failed')).toBeVisible();
   await expect(page.getByText('10 passed')).toBeVisible();
   await expect(page.getByText('18 executed')).toBeVisible();
-});
 
-test('meta', async ({ page }) => {
   await expect(page.locator('dl').getByText('node.js')).toBeVisible();
   await expect(page.locator('dl').getByText('playwright-bdd')).toBeVisible();
+
+  await expect(getFeature(page).getTags()).toHaveText(['@feature-tag']);
 });
