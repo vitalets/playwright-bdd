316803484b26f02534588e3a276cc85928518077
Vitaliy Potapov <noginsk@rambler.ru>
2024-02-04 13:20:03 +0400

    reporter-data: add test for hook locations

--- test/reporter-data/test.mjs
+++ test/reporter-data/test.mjs
@@ -4,11 +4,21 @@ import { test, getTestName, execPlaywrightTest } from '../helpers.mjs';
 
 test(getTestName(import.meta), (t) => {
   const stdout = execPlaywrightTest(t.name);
-  expect(stdout).toContain(
-    `Given I am on home page ${normalize('.features-gen/sample.feature.spec.js')}:7:11`,
-  );
-  expect(stdout).toContain('page.goto(https://example.com) fixtures.ts:8:21');
+  checkStepLocations(stdout);
+  checkStepTitles(stdout);
+});
 
+function checkStepLocations(stdout) {
+  expect(stdout).toContain(
+    `Given I am on home page ${normalize('.features-gen/features/sample.feature.spec.js')}:7:11`,
+  );
+  expect(stdout).toContain(
+    `page.goto(https://example.com) ${normalize('features/fixtures.ts')}:8:21`,
+  );
+  expect(stdout).toContain(`hook 1 ${normalize('features/steps.ts')}:13:1`);
+}
+
+function checkStepTitles(stdout) {
   // prepended keywords
   expect(stdout).toContain(`And Action 1`);
   expect(stdout).toContain(`When Action 2`);
@@ -20,4 +30,4 @@ test(getTestName(import.meta), (t) => {
   // for non-en lang keywords are not prepended
   expect(stdout).toContain(`Состояние 1`);
   expect(stdout).not.toContain(`Пусть Состояние 1`);
-});
+}
