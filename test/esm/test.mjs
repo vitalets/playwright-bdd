178a263f5bb15ac67980dc386ed8856d567c71a7
Vitaliy Potapov <noginsk@rambler.ru>
2024-04-01 12:22:13 +0400

    test: try run esm on win

--- test/esm/test.mjs
+++ test/esm/test.mjs
@@ -3,7 +3,8 @@ import { test, TestDir, execPlaywrightTest } from '../_helpers/index.mjs';
 // Skip on CI for Win as it fails with error:
 // TypeError: Invalid module "..\\..\\steps\\fixtures.js" is not a valid package name
 // todo: investigate
-const skip = process.env.CI && process.platform === 'win32';
+// try run esm on win
+const skip = false; // process.env.CI && process.platform === 'win32';
 
 const testDir = new TestDir(import.meta);
 
