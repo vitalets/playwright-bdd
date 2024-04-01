8bb97e7aad13450e5e7312b67f348e12f6e7219f
Vitaliy Potapov <noginsk@rambler.ru>
2024-04-01 12:40:48 +0400

    test: try to fix reporter test on ci win

--- test/reporter-cucumber-html/playwright.config.ts
+++ test/reporter-cucumber-html/playwright.config.ts
@@ -19,7 +19,7 @@ export default defineConfig({
     screenshot: 'only-on-failure',
   },
   // don't decrease this timeout as it's important for timeout tests
-  timeout: 1500,
+  timeout: process.env.CI && process.platform === 'win32' ? 2000 : 1500,
   expect: {
     timeout: 1,
   },
