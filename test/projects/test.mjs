d16ee8d096882800c8d8654566a8e6206e3db49f
Vitaliy Potapov <noginsk@rambler.ru>
2024-04-05 18:53:25 +0400

    test: simplify projects test

--- test/projects/test.mjs
+++ test/projects/test.mjs
@@ -1,8 +1,7 @@
-import { test, TestDir, execPlaywrightTest, DEFAULT_CMD } from '../_helpers/index.mjs';
+import { test, TestDir, execPlaywrightTest } from '../_helpers/index.mjs';
 
 const testDir = new TestDir(import.meta);
 
 test(testDir.name, () => {
   execPlaywrightTest(testDir.name);
-  execPlaywrightTest(testDir.name, `${DEFAULT_CMD} --project=project-two`);
 });
