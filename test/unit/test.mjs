64ccfe528258c3b0f8cce897388995b0a11590fd
Vitaliy Potapov <noginsk@rambler.ru>
2024-04-09 10:30:18 +0400

    test: fix unit test on win

--- test/unit/test.mjs
+++ test/unit/test.mjs
@@ -1,11 +1,12 @@
 import path from 'node:path';
+import { pathToFileURL } from 'node:url';
 import { test, expect, TestDir } from '../_helpers/index.mjs';
 
 const testDir = new TestDir(import.meta);
 
 test(`${testDir.name} (finalizePattern)`, async () => {
   const modulePath = path.resolve('dist/utils/paths.js');
-  const { finalizePattern } = await import(modulePath);
+  const { finalizePattern } = await import(pathToFileURL(modulePath));
 
   expect(finalizePattern('a/steps', 'js')).toEqual('a/steps/**/*.js');
   expect(finalizePattern('a/steps/', 'js')).toEqual('a/steps/**/*.js');
