c76dac3377d3024d1ecb8065ab8c5ea41c632156
Vitaliy Potapov <noginsk@rambler.ru>
2024-04-12 17:49:26 +0400

    test: fix duplicate-steps for win

--- test/duplicate-steps/test.mjs
+++ test/duplicate-steps/test.mjs
@@ -12,9 +12,9 @@ const featureFile = normalize('features/sample.feature');
 test(`${testDir.name} (main thread - regular steps)`, () => {
   const error = [
     `Multiple step definitions matched for text: "duplicate step" (${featureFile})`,
-    `  duplicate step - steps/steps.ts:6`,
-    `  duplicate step - steps/steps.ts:7`,
-    `  duplicate step - steps/steps.ts:8`,
+    `  duplicate step - ${normalize('steps/steps.ts')}:6`,
+    `  duplicate step - ${normalize('steps/steps.ts')}:7`,
+    `  duplicate step - ${normalize('steps/steps.ts')}:8`,
   ].join('\n');
   execPlaywrightTestWithError(testDir.name, error, {
     cmd: BDDGEN_CMD,
@@ -25,9 +25,9 @@ test(`${testDir.name} (main thread - regular steps)`, () => {
 test(`${testDir.name} (main thread - decorator steps)`, () => {
   const error = [
     `Multiple step definitions matched for text: "duplicate decorator step" (${featureFile})`,
-    `  duplicate decorator step - steps/TodoPage.ts:7`,
-    `  duplicate decorator step - steps/TodoPage.ts:10`,
-    `  duplicate decorator step - steps/TodoPage.ts:13`,
+    `  duplicate decorator step - ${normalize('steps/TodoPage.ts')}:7`,
+    `  duplicate decorator step - ${normalize('steps/TodoPage.ts')}:10`,
+    `  duplicate decorator step - ${normalize('steps/TodoPage.ts')}:13`,
   ].join('\n');
   execPlaywrightTestWithError(testDir.name, error, {
     cmd: BDDGEN_CMD,
@@ -38,9 +38,9 @@ test(`${testDir.name} (main thread - decorator steps)`, () => {
 test(`${testDir.name} (worker - regular steps)`, () => {
   const error = [
     `Multiple step definitions matched for text: "duplicate step" (${featureFile})`,
-    `  duplicate step - steps/steps.ts:6`,
-    `  duplicate step - steps/steps.ts:7`,
-    `  duplicate step - steps/steps.ts:8`,
+    `  duplicate step - ${normalize('steps/steps.ts')}:6`,
+    `  duplicate step - ${normalize('steps/steps.ts')}:7`,
+    `  duplicate step - ${normalize('steps/steps.ts')}:8`,
   ].join('\n');
   execPlaywrightTestWithError(testDir.name, error, {
     cmd: BDDGEN_CMD,
