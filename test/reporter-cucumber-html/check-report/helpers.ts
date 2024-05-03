b368ea33c4983c0377f9dac5d2d80d03d04d73ed
Vitaliy Potapov <noginsk@rambler.ru>
2024-05-03 10:38:22 +0400

    test: improveget errors in repoter html

--- test/reporter-cucumber-html/check-report/helpers.ts
+++ test/reporter-cucumber-html/check-report/helpers.ts
@@ -82,7 +82,7 @@ export class Scenario {
     return this.getSteps()
       .locator('div > pre')
       .filter({
-        hasText: /error|timeout|expected|browser has been closed/i,
+        hasText: /error|timeout|expected|browser .*?closed/i,
       });
   }
 
