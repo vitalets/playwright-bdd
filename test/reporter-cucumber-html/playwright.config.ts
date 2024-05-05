f7e7c75f572949eff700ce81b6cc906e032c97da
Vitaliy Potapov <noginsk@rambler.ru>
2024-05-05 11:54:31 +0400

    set test timeout 3000 for reporter html

--- test/reporter-cucumber-html/playwright.config.ts
+++ test/reporter-cucumber-html/playwright.config.ts
@@ -21,7 +21,7 @@ export default defineConfig({
   },
   // don't reduce test timeout as it produces unreliable errors
   // if define this timeout as @timeout tag, timeout in after fixture does not work
-  timeout: 1500,
+  timeout: 3000,
   expect: {
     timeout: 1,
   },
