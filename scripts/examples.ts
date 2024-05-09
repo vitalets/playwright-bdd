462deb069a72d66525708fce11efd5eafe2daa66
Vitaliy Potapov <noginsk@rambler.ru>
2024-05-09 16:01:24 +0400

    examples: refactor to single file and not creating playwright-bdd in root node_modules

--- scripts/examples.ts
+++ scripts/examples.ts
@@ -8,15 +8,23 @@
  */
 
 import path from 'node:path';
-import { execSync } from 'node:child_process';
-import { buildAndInstallPlaywrightBdd } from './helpers';
+import fs from 'node:fs';
+import { ExecSyncOptions, execSync } from 'node:child_process';
+import pkg from '../package.json';
 
+const isCI = Boolean(process.env.CI);
 const dir = process.argv[2];
-const dirs = dir ? [dir] : ['basic', 'cucumber-style', 'decorators', 'esm'];
+const dirs = dir
+  ? [dir]
+  : [
+      'basic', // prettier-ignore
+      'cucumber-style',
+      'decorators',
+      'esm',
+    ];
+const generatedTar = `playwright-bdd-${pkg.version}.tgz`;
 
-buildAndInstallPlaywrightBdd({
-  isCI: Boolean(process.env.CI),
-});
+buildAndInstallPlaywrightBdd();
 runExamples();
 
 function runExamples() {
@@ -25,4 +33,21 @@ function runExamples() {
     console.log(cwd); // eslint-disable-line no-console
     execSync(`npm test`, { cwd, stdio: 'inherit' });
   });
+}
+
+function buildAndInstallPlaywrightBdd() {
+  try {
+    runCmd('npm run build');
+    runCmd('npm pack --loglevel=error');
+    // on CI remove node_modules to check that playwright-bdd brings all needed dependencies
+    if (isCI) fs.rmSync('node_modules', { recursive: true });
+    runCmd(`npm install --no-save ../${generatedTar}`, { cwd: 'examples' });
+  } finally {
+    fs.rmSync(generatedTar, { force: true });
+  }
+}
+
+function runCmd(cmd: string, opts: ExecSyncOptions = {}) {
+  console.log(`Running: ${cmd}`); // eslint-disable-line no-console
+  execSync(cmd, { ...opts, stdio: 'inherit' });
 }
