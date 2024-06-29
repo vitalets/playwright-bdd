dc7c5a7e06f9e327465ca0182d369b746a0332a3
Vitaliy Potapov <noginsk@rambler.ru>
2024-06-29 13:07:26 +0400

    fix examples

--- scripts/examples.mjs
+++ scripts/examples.mjs
@@ -13,7 +13,6 @@ import { execSync } from 'node:child_process';
 import fg from 'fast-glob';
 
 const pkg = JSON.parse(fs.readFileSync('./package.json'));
-const isCI = Boolean(process.env.CI);
 const dir = process.argv[2];
 const dirs = dir ? [dir] : getExampleDirs();
 const generatedTar = `playwright-bdd-${pkg.version}.tgz`;
@@ -36,8 +35,7 @@ function buildAndInstallPlaywrightBdd() {
     // todo: on CI remove node_modules to check that playwright-bdd brings all needed dependencies
     // isCI && fs.rmSync('node_modules', { recursive: true });
     // isCI && runCmd(`npm install @playwright/test @cucumber/cucumber cross-env ts-node`, { cwd: 'examples' });
-    runCmd(`npm install --legacy-peer-deps --no-save ../${generatedTar}`, { cwd: 'examples' });
-    isCI && runCmd(`npx playwright install --with-deps chromium`, { cwd: 'examples' });
+    runCmd(`npm install --omit=peer --no-save ../${generatedTar}`, { cwd: 'examples' });
   } finally {
     fs.rmSync(generatedTar, { force: true });
   }
