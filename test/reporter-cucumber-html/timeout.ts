0fcd6ae1589cd5ed9a341d1043dba10fecaccfbb
Vitaliy Potapov <noginsk@rambler.ru>
2024-06-29 12:58:59 +0400

    test: increase reporter test timeout on win

--- test/reporter-cucumber-html/timeout.ts
+++ test/reporter-cucumber-html/timeout.ts
@@ -2,4 +2,4 @@
 // keep in separate file, b/c it is also used in test/reporter-cucumber-merge
 // don't reduce test timeout as it produces unreliable errors
 // if define this timeout as @timeout tag, timeout in after fixture does not work
-export const testTimeout = process.platform === 'win32' ? 3500 : 1500;
+export const testTimeout = process.platform === 'win32' ? 4000 : 1500;
