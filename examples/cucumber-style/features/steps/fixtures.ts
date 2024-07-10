5d828de8e3e1f44f4d7d3edcedd48ba219fc3105
Vitaliy Potapov <noginsk@rambler.ru>
2024-07-10 17:44:33 +0400

    update cucumber style examples

--- examples/cucumber-style/features/steps/fixtures.ts
+++ examples/cucumber-style/features/steps/fixtures.ts
@@ -2,7 +2,10 @@ import { test as base, createBdd } from 'playwright-bdd';
 import { World } from './world';
 
 export const test = base.extend<{ world: World }>({
-  world: ({ page }, use, testInfo) => use(new World(page, testInfo)),
+  world: async ({ page }, use, testInfo) => {
+    const world = new World(page, testInfo);
+    await use(world);
+  },
 });
 
 export const { Given, When, Then } = createBdd(test, { worldFixture: 'world' });
