cf87cead73a1e3d061310b01fc940e1c77423707
Vitaliy Potapov <noginsk@rambler.ru>
2024-08-03 14:25:39 +0400

    test: prepare test for custom test empty

---
+++ test/types-non-strict/steps/custom-test-empty.ts
@@ -0,0 +1,12 @@
+// import { Page } from '@playwright/test';
+// import { test as base, createBdd } from 'playwright-bdd';
+// import { expectTypeOf } from 'expect-type';
+
+// const test = base.extend({});
+
+// const { Given } = createBdd(test);
+
+// Given('some step', async ({ page, $tags }) => {
+//   expectTypeOf($tags).toEqualTypeOf<string[]>();
+//   expectTypeOf(page).toEqualTypeOf<Page>();
+// });
