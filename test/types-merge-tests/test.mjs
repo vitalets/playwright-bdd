f18999dca85257be021452553323956819b63637
Vitaliy Potapov <noginsk@rambler.ru>
2024-11-22 14:08:38 +0400

    test: upd tests for min pw 1.39

--- test/types-merge-tests/test.mjs
+++ test/types-merge-tests/test.mjs
@@ -1,12 +1,8 @@
-import { test, TestDir, execPlaywrightTest, playwrightVersion } from '../_helpers/index.mjs';
+import { test, TestDir, execPlaywrightTest } from '../_helpers/index.mjs';
 
 const testDir = new TestDir(import.meta);
 
-// mergeTests was added in pw 1.39
-// See: https://playwright.dev/docs/release-notes#merge-test-fixtures
-const skip = playwrightVersion < '1.39';
-
-test(testDir.name, { skip }, () => {
+test(testDir.name, () => {
   execPlaywrightTest(testDir.name, {
     cmd: 'npx tsc',
   });
