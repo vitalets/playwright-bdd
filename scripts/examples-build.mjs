5d8bc21cdab9e845af4040b3cf65b3cb9a85cce9
Vitaliy Potapov <noginsk@rambler.ru>
2024-12-03 11:19:15 +0400

    pkg: add examples:build

---
+++ scripts/examples-build.mjs
@@ -0,0 +1,32 @@
+/**
+ * Build playwright-bdd to run examples.
+ * npm run examples:build
+ */
+
+import fs from 'node:fs';
+import { execSync } from 'node:child_process';
+
+const pkg = JSON.parse(fs.readFileSync('./package.json'));
+const isCI = Boolean(process.env.CI);
+const generatedTar = `playwright-bdd-${pkg.version}.tgz`;
+
+buildAndInstallPlaywrightBdd();
+
+function buildAndInstallPlaywrightBdd() {
+  try {
+    runCmd('npm run build');
+    runCmd('npm pack --loglevel=error');
+    // on CI remove node_modules to check that playwright-bdd brings all needed dependencies
+    if (isCI) fs.rmSync('node_modules', { recursive: true });
+    if (!isCI) runCmd(`npm install --omit=peer --no-save ../${generatedTar}`, { cwd: 'examples' });
+    if (isCI) runCmd(`npm install --no-save ../${generatedTar}`, { cwd: 'examples' });
+    if (isCI) runCmd(`npx playwright install --with-deps chromium`, { cwd: 'examples' });
+  } finally {
+    fs.rmSync(generatedTar, { force: true });
+  }
+}
+
+function runCmd(cmd, opts = {}) {
+  console.log(`Running: ${cmd}`); // eslint-disable-line no-console
+  execSync(cmd, { ...opts, stdio: 'inherit' });
+}
--- scripts/examples.mjs
+++ scripts/examples.mjs
@@ -8,44 +8,21 @@
  */
 
 import path from 'node:path';
-import fs from 'node:fs';
 import { execSync } from 'node:child_process';
 import fg from 'fast-glob';
 
-const pkg = JSON.parse(fs.readFileSync('./package.json'));
-const isCI = Boolean(process.env.CI);
 const dir = process.argv[2];
 const dirs = dir ? [dir] : getExampleDirs();
-const generatedTar = `playwright-bdd-${pkg.version}.tgz`;
+const logger = console;
 
-buildAndInstallPlaywrightBdd();
 runExamples();
 
 function runExamples() {
   dirs.forEach((dir) => {
     const cwd = path.join('examples', dir);
-    console.log(cwd); // eslint-disable-line no-console
+    logger.log(cwd);
     execSync(`npm test`, { cwd, stdio: 'inherit' });
   });
-}
-
-function buildAndInstallPlaywrightBdd() {
-  try {
-    runCmd('npm run build');
-    runCmd('npm pack --loglevel=error');
-    // on CI remove node_modules to check that playwright-bdd brings all needed dependencies
-    if (isCI) fs.rmSync('node_modules', { recursive: true });
-    if (!isCI) runCmd(`npm install --omit=peer --no-save ../${generatedTar}`, { cwd: 'examples' });
-    if (isCI) runCmd(`npm install --no-save ../${generatedTar}`, { cwd: 'examples' });
-    if (isCI) runCmd(`npx playwright install --with-deps chromium`, { cwd: 'examples' });
-  } finally {
-    fs.rmSync(generatedTar, { force: true });
-  }
-}
-
-function runCmd(cmd, opts = {}) {
-  console.log(`Running: ${cmd}`); // eslint-disable-line no-console
-  execSync(cmd, { ...opts, stdio: 'inherit' });
 }
 
 function getExampleDirs() {
