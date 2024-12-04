ea2c204b4a25372a5b11f6c223f0fa59f8941c3b
Vitaliy Potapov <noginsk@rambler.ru>
2024-12-04 12:56:22 +0400

    test: fix reporter data ignored files

--- test/reporter-data/test.mjs
+++ test/reporter-data/test.mjs
@@ -10,9 +10,12 @@ const testDir = new TestDir(import.meta);
 
 // Since PW 1.46 there is a 'box' option for the fixture, that hides it from the report.
 // Till Pw 1.41 first trace was attached to BeforeAll hooks, not to top level test.steps.
-const expectedDir =
-  playwrightVersion < '1.42' ? 'till-1.41' : playwrightVersion < '1.46' ? 'till-1.45' : 'current';
-const expectedReportPath = `expected-reports/${expectedDir}/report.txt`;
+const expectedReportName =
+  playwrightVersion < '1.42'
+    ? 'report-till-1.42.txt'
+    : playwrightVersion < '1.46'
+      ? 'report-till-1.46.txt'
+      : 'report-current.txt';
 
 test(testDir.name, () => {
   testDir.clearDir('actual-reports');
@@ -23,7 +26,7 @@ test(testDir.name, () => {
 });
 
 function checkStepsTree() {
-  const expectedTree = testDir.getFileContents(expectedReportPath);
+  const expectedTree = testDir.getFileContents(`expected-reports/${expectedReportName}`);
   const actualTree = testDir.getFileContents('actual-reports/report.txt');
   expect(actualTree).toEqual(expectedTree);
 }
