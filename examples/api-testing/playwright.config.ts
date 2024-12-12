a9f0f71f270364d92c84a223671489fa51aa9e73
vitalets <noginsk@rambler.ru>
2024-12-12 18:37:19 +0400

    examples: api testing add api key sample

--- examples/api-testing/playwright.config.ts
+++ examples/api-testing/playwright.config.ts
@@ -10,6 +10,12 @@ export default defineConfig({
   testDir,
   reporter: [cucumberReporter('html', { outputFile: 'cucumber-report/index.html' })],
   use: {
+    // All requests we send go to this API endpoint.
     baseURL: 'https://jsonplaceholder.typicode.com',
+    extraHTTPHeaders: {
+      // Add authorization token to all requests.
+      // Assuming personal access token available in the environment.
+      // Authorization: `token ${process.env.API_TOKEN}`,
+    },
   },
 });
