b6fdfda13968958f81a6953927c5633026333ae0
vitalets <noginsk@rambler.ru>
2024-12-13 10:22:36 +0400

    test: fix html reporter for before all timeout

--- test/reporter-cucumber-html/features/failing-before-all/specs/timeout.feature.spec.ts
+++ test/reporter-cucumber-html/features/failing-before-all/specs/timeout.feature.spec.ts
@@ -1,10 +1,11 @@
-import { normalize } from 'node:path';
 import { expect } from '@playwright/test';
 import { test } from '../../../check-report/fixtures';
 
 test('scenario 1', async ({ scenario }) => {
   await expect(scenario.getSteps()).toContainText([
-    `Hook "my timeouted hook" failed: ${normalize('features/failing-before-all/steps.ts')}:`, // prettier-ignore
+    // Sometimes we still have 'BeforeAll Hooks' here, not 'my timeouted hook'.
+    // It is when 'duration' != -1 -> no way to find exact timeouted item.
+    /Hook "(my timeouted hook|BeforeAll Hooks)" failed/, // prettier-ignore
     'GivenAction 1',
   ]);
   await expect(scenario.getErrors()).toContainText([
