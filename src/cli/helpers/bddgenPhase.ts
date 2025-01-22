c9bd57d908a612e9210986e550bf11b10a1aa413
vitalets <noginsk@rambler.ru>
2025-01-22 16:40:48 +0400

    make knip happy

--- src/cli/helpers/bddgenPhase.ts
+++ src/cli/helpers/bddgenPhase.ts
@@ -1,6 +1,7 @@
 /**
  * During bddgen phase there is env variable PLAYWRIGHT_BDD_GEN set to '1'.
  * Keep this file separate as it is imported in workers -> no unneeded dependencies.
+ * @public
  */
 export function isBddGenPhase() {
   return Boolean(process.env.PLAYWRIGHT_BDD_GEN);
