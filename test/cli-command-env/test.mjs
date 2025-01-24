1d3e4aebe276ff0142a498338e3819bad8dc7b89
vitalets <noginsk@rambler.ru>
2025-01-24 11:34:31 +0400

    cli: env: show none for missing playwright config

--- test/cli-command-env/test.mjs
+++ test/cli-command-env/test.mjs
@@ -5,7 +5,16 @@ const testDir = new TestDir(import.meta);
 test(testDir.name, () => {
   const stdout = execPlaywrightTest(testDir.name, `${BDDGEN_CMD} env`);
   expect(stdout).toContain('platform:');
-  expect(stdout).toContain('playwright-bdd:');
+  expect(stdout).toContain(`node: ${process.version}`);
+  expect(stdout).toContain('playwright-bdd: v');
   expect(stdout).toContain('@playwright/test: v1.');
   expect(stdout).toContain('Playwright config file: playwright.config.ts');
+});
+
+test(`${testDir.name} (no playwright config)`, () => {
+  // run in 'features' dir to miss playwright.config.ts
+  const cmd = `${BDDGEN_CMD} env`.replace('../node_modules', '../../node_modules');
+  const stdout = execPlaywrightTest(`${testDir.name}/features`, cmd);
+
+  expect(stdout).toContain('Playwright config file: none');
 });
