c542f1babec02d2a99713f1d9cab25b6f51fe913
vitalets <noginsk@rambler.ru>
2025-02-21 19:25:07 +0400

    scripts: add note to build

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,6 +7,8 @@ rm -rf ./dist
 npx tsc -p tsconfig.build.json
 
 # copy 'dist' to 'node_modules/playwright-bdd' to allow import from 'playwright-bdd' in tests
+# todo: better to copy to 'test/node_modules/playwright-bdd', 
+# as from the root node_modules this package is cleared on every npm install <some-package>
 rm -rf ./node_modules/playwright-bdd
 mkdir -p ./node_modules/playwright-bdd
 cp -R ./dist ./node_modules/playwright-bdd/
