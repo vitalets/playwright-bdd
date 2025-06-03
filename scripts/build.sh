5f952856732be84e6bbbbafca42d586b10aaff46
vitalets <noginsk@rambler.ru>
2025-06-03 19:38:14 +0400

    add comments to build script

--- scripts/build.sh
+++ scripts/build.sh
@@ -7,8 +7,10 @@ rm -rf ./dist
 npx tsc -p tsconfig.build.json
 
 # copy 'dist' to 'node_modules/playwright-bdd' to allow import from 'playwright-bdd' in tests
-# todo: better to copy to 'test/node_modules/playwright-bdd', 
-# as from the root node_modules this package is cleared on every npm install <some-package>
+# note: if copied to 'test/node_modules/playwright-bdd', 
+# TS finds playwright-bdd typings in 'examples/node_modules/playwright-bdd' 
+# instead of 'test/node_modules/playwright-bdd'. 
+# If types change, TS may show type errors in tests until examples re-build (it's unconvenient).
 rm -rf ./node_modules/playwright-bdd
 mkdir -p ./node_modules/playwright-bdd
 cp -R ./dist ./node_modules/playwright-bdd/
