ff302d0666fc1c38af47f4fa9c46d959f4a57259
vitalets <noginsk@rambler.ru>
2025-11-23 14:35:41 +0100

    test: fix snapshot tests for pw 1.57

--- test/reporter-cucumber-ext-attachments/check-report/playwright.config.ts
+++ test/reporter-cucumber-ext-attachments/check-report/playwright.config.ts
@@ -13,6 +13,7 @@ export default defineConfig({
     baseURL,
     screenshot: { mode: 'only-on-failure', fullPage: true },
     viewport: { width: 800, height: 720 },
+    trace: 'retain-on-failure',
   },
   webServer: {
     command: `npx http-server actual-reports -c-1 -a localhost -p ${process.env.PORT}`,
