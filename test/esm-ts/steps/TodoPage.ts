61b3efd71aafd0b9897ba3b8683ee49d2b27a592
vitalets <noginsk@rambler.ru>
2026-03-13 13:46:08 +0100

    ci: try to fix test for win

--- test/esm-ts/steps/TodoPage.ts
+++ test/esm-ts/steps/TodoPage.ts
@@ -1,9 +1,8 @@
 import { Fixture, Given } from 'playwright-bdd/decorators';
 import type { test } from './fixtures2.js';
 
-export
-@Fixture<typeof test>('todoPage')
-class TodoPage {
+// prettier-ignore
+export @Fixture<typeof test>('todoPage') class TodoPage {
   @Given('TodoPage: step')
   async step() {}
 }
